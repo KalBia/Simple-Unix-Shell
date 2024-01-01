f92d323685ede722f175878c80535962d692f489
KalBia <340152@uwr.edu.pl>
2024-01-01 20:20:57 +0100

    Revert "First version of command.c jobs.c lexer.c shell.c - before first push"
    This reverts commit 8a7c9b71756581a5ad148df3e6f7a8fe198d0bda.

--- jobs.c
+++ jobs.c
@@ -20,15 +20,6 @@ static int njobmax = 1;             /* number of slots in jobs array */
 static int tty_fd = -1;             /* controlling terminal file descriptor */
 static struct termios shell_tmodes; /* saved shell terminal modes */
 
-/* DESCRIPTION:
- * If child-process changes its status - finished, stopped, continued - it sends SIGCHLD to parent-process,
- * so we need to handle this case.
- * Moreover we have to check, if it changes status of the whole job.
- * INPUT:
- * int sig - just a convention
- * OUTPUT:
- * none */
-
 static void sigchld_handler(int sig) {
   int old_errno = errno;
   pid_t pid;
@@ -36,68 +27,6 @@ static void sigchld_handler(int sig) {
   /* TODO: Change state (FINISHED, RUNNING, STOPPED) of processes and jobs.
    * Bury all children that finished saving their status in jobs. */
 #ifdef STUDENT
-
-  /* receive all the signals from processess 
-   * WNOHANG - return if no more signals
-   * WUNTRACED - receive stop signals
-   * WCONTINUED - receive continue signals */
-  while ((pid = waitpid(WAIT_ANY, &status, WNOHANG | WUNTRACED | WCONTINUED)) > 0)
-  {
-    /* job by job */
-    for (int j = FG; j < njobmax; j++)
-    {
-      job_t *job = &jobs[j];
-      if (job->pgid == 0) /* free slot - ignore it */
-        continue;
-
-      /* variables to check if we need to change the job's status */
-      bool changeForRunning = false; /* Any running process? == status->RUNNING */
-      bool changeForStopped = false; /* Any stopped process? AND none running processes == status->STOPPED*/
-
-      for (int i = 0; i < job->nproc; i++) /* process by process */
-      {
-        if (job->proc[i].state == FINISHED) /* we don't need to do anything with it anymore*/
-          continue;
-
-        if (job->proc[i].pid == pid) /* our process from waitpid that we need to change */
-        {
-          job->proc[i].exitcode = -1; /* forewarned is forearmed */
-
-          if (WIFEXITED(status)) /* if terminated normally */
-          {
-            job->proc[i].state = FINISHED;
-            job->proc[i].exitcode = WEXITSTATUS(status);
-          }
-          else if (WIFSIGNALED(status)) /* if killed by signal */
-          {
-            job->proc[i].state = FINISHED;
-            job->proc[i].exitcode = WTERMSIG(status);
-          }
-          else if (WIFSTOPPED(status)) /* if stopped */
-          {
-            job->proc[i].state = STOPPED;
-          }
-          else if (WIFCONTINUED(status)) /* if continued */
-          {
-            job->proc[i].state = RUNNING;
-          }
-        }
-
-        if (job->proc[i].state == RUNNING)
-          changeForRunning = true;
-        else if (job->proc[i].state == STOPPED)
-          changeForStopped = true;
-      }
-
-      if (changeForRunning) /* we have at least one running process in job */
-        job->state = RUNNING;
-      else if (changeForStopped) /* we don't have any running process in job, but at least one is stopped */
-        job->state = STOPPED;
-      else /* we don't have any running or stopped process == all are finished */
-        job->state = FINISHED;
-    }
-  }
-
   (void)status;
   (void)pid;
 #endif /* !STUDENT */
@@ -108,13 +37,6 @@ static void sigchld_handler(int sig) {
 static int exitcode(job_t *job) {
   return job->proc[job->nproc - 1].exitcode;
 }
-
-/* DESCRIPTION:
- * Find an empty slot in jobs' array - make it bigger if needed
- * INPUT:
- * none
- * OUTPUT:
- * static int - index of allocated job in array */
 
 static int allocjob(void) {
   /* Find empty slot for background job. */
@@ -128,26 +50,11 @@ static int allocjob(void) {
   return njobmax++;
 }
 
-/* DESCRIPTION:
- * Find place for process in the processes' array in job nr j
- * INPUT:
- * int j - number of job that process works for
- * OUTPUT:
- * static int - index of allocated process in array */
-
 static int allocproc(int j) {
   job_t *job = &jobs[j];
   job->proc = realloc(job->proc, sizeof(proc_t) * (job->nproc + 1));
   return job->nproc++;
 }
-
-/* DESCRIPTION:
- * Initialize new job in jobs' array
- * INPUT:
- * pid_t pgid - job's process group ID
- * int bg - foreground (0) or background (1)
- * OUTPUT:
- * int - index of job in jobs' array */
 
 int addjob(pid_t pgid, int bg) {
   int j = bg ? allocjob() : FG;
@@ -162,16 +69,9 @@ int addjob(pid_t pgid, int bg) {
   return j;
 }
 
-/* DESCRIPTION:
- * Empty the slot in jobs' array to reuse it
- * INPUT:
- * job_t *job - pointer to the job to delete
- * OUTPUT:
- * none */
-
 static void deljob(job_t *job) {
-  assert(job->state == FINISHED);     /* delete only if the job is finished */
-  free(job->command);                 /* clean up */
+  assert(job->state == FINISHED);
+  free(job->command);
   free(job->proc);
   job->pgid = 0;
   job->command = NULL;
@@ -179,27 +79,11 @@ static void deljob(job_t *job) {
   job->nproc = 0;
 }
 
-/* DESCRIPTION:
- * Move job from one slot to another
- * INPUT:
- * int from - number of slot from which we move job
- * int to - number of slot to which we move job
- * OUTPUT:
- * none */
-
 static void movejob(int from, int to) {
-  assert(jobs[to].pgid == 0);                     /* only if the slot "to" is empty */
+  assert(jobs[to].pgid == 0);
   memcpy(&jobs[to], &jobs[from], sizeof(job_t));
   memset(&jobs[from], 0, sizeof(job_t));
 }
-
-/* DESCRIPTION:
- * Make string with command from shell - uses strapp from lexer.c
- * INPUT:
- * char **cmdp - pointer to poiner to where we want to have the string
- * char **argv - arguments as we put them in execve
- * OUTPUT:
- * none */
 
 static void mkcommand(char **cmdp, char **argv) {
   if (*cmdp)
@@ -211,37 +95,21 @@ static void mkcommand(char **cmdp, char **argv) {
   }
 }
 
-/* DESCRIPTION:
- * Add process to the processes' array in job
- * INPUT:
- * int j - number of job, "owner" of process
- * pid_t pid - pid of process to add
- * char **argv - args like in execve
- * OUTPUT:
- * none */
-
 void addproc(int j, pid_t pid, char **argv) {
-  assert(j < njobmax);                  /* is number of job correct? */
+  assert(j < njobmax);
   job_t *job = &jobs[j];
 
-  int p = allocproc(j);                 /* get free index in array */
+  int p = allocproc(j);
   proc_t *proc = &job->proc[p];
   /* Initial state of a process. */
   proc->pid = pid;
   proc->state = RUNNING;
   proc->exitcode = -1;
-  mkcommand(&job->command, argv);       /* command from shell */
+  mkcommand(&job->command, argv);
 }
 
-/* DESCRIPTION:
- * Returns job's state.
- * If it's finished, delete it and return exitcode through statusp.
- * INPUT:
- * int j - number of job
- * int *statusp - pointer to place for exitcode, if state == finished
- * OUTPUT:
- * static int - job's state */
-
+/* Returns job's state.
+ * If it's finished, delete it and return exitcode through statusp. */
 static int jobstate(int j, int *statusp) {
   assert(j < njobmax);
   job_t *job = &jobs[j];
@@ -249,25 +117,11 @@ static int jobstate(int j, int *statusp) {
 
   /* TODO: Handle case where job has finished. */
 #ifdef STUDENT
-
-  if (state == FINISHED)
-  {
-    *statusp = exitcode(job);       /* get the job's status */
-    deljob(job);                    /* clean up the job */
-  }
-
   (void)exitcode;
 #endif /* !STUDENT */
 
   return state;
 }
-
-/* DESCRIPTION:
- * Return command of job
- * INPUT:
- * int j - number of job
- * OUTPUT:
- * char* - command */
 
 char *jobcmd(int j) {
   assert(j < njobmax);
@@ -275,18 +129,10 @@ char *jobcmd(int j) {
   return job->command;
 }
 
-/* DESCRIPTION:
- * Continues a job that has been stopped. If move to foreground was requested,
- * then move the job to foreground and start monitoring it.
- * INPUT:
- * int j - number of job, can be -1 => we take the job with highest number
- * int bg - foreground (1) or background (1)
- * sigset_t *mask
- * OUTPUT:
- * bool - success? */
-
+/* Continues a job that has been stopped. If move to foreground was requested,
+ * then move the job to foreground and start monitoring it. */
 bool resumejob(int j, int bg, sigset_t *mask) {
-  if (j < 0) {       /* if j not given, take job with highest number */
+  if (j < 0) {
     for (j = njobmax - 1; j > 0 && jobs[j].state == FINISHED; j--)
       continue;
   }
@@ -296,47 +142,13 @@ bool resumejob(int j, int bg, sigset_t *mask) {
 
     /* TODO: Continue stopped job. Possibly move job to foreground slot. */
 #ifdef STUDENT
-
-  job_t *job = &jobs[j];
-  job->state = RUNNING;
-
-  /* Explanation:
-   * negative pgid sends signal to all processes in group
-   * SIGCONT - signal to continue process */
-
-  /* foreground job */
-  if (!bg)
-  {
-    movejob(j, 0);
-
-    /* set as foreground process group (for example for cat - it would stop again immediately otherwise)*/
-    setfgpgrp(jobs[0].pgid);
-    Tcsetattr(tty_fd, 0, &shell_tmodes);
-
-    Kill(-jobs[0].pgid, SIGCONT);
-
-    msg("[%d] continue '%s'\n", j, jobcmd(0));
-    (void)monitorjob(mask);
-  }
-  else
-  {
-    Kill(-(job->pgid), SIGCONT);
-    msg("[%d] continue '%s'\n", j, jobcmd(j));
-  }
-
   (void)movejob;
 #endif /* !STUDENT */
 
   return true;
 }
 
-/* DESCRIPTION:
- * Kill the job by sending it a SIGTERM.
- * INPUT:
- * int j - number of job
- * OUTPUT:
- * bool - success? */
-
+/* Kill the job by sending it a SIGTERM. */
 bool killjob(int j) {
   if (j >= njobmax || jobs[j].state == FINISHED)
     return false;
@@ -344,26 +156,12 @@ bool killjob(int j) {
 
   /* TODO: I love the smell of napalm in the morning. */
 #ifdef STUDENT
-
-  job_t *job = &jobs[j];
-
-  /* negative pgid - kill all processes in group */
-  Kill(-(job->pgid), SIGTERM);
-  if (job->state == STOPPED)
-    Kill(-(job->pgid), SIGCONT); /* process has to be concious, to be killed */
-
 #endif /* !STUDENT */
 
   return true;
 }
 
-/* DESCRIPTION:
- * Report state of requested background jobs. Clean up finished jobs.
- * INPUT:
- * int which - ALL / FINISHED / RUNNING / STOPPED
- * OUTPUT:
- * none */
-
+/* Report state of requested background jobs. Clean up finished jobs. */
 void watchjobs(int which) {
   for (int j = BG; j < njobmax; j++) {
     if (jobs[j].pgid == 0)
@@ -371,74 +169,18 @@ void watchjobs(int which) {
 
       /* TODO: Report job number, state, command and exit code or signal. */
 #ifdef STUDENT
-
-    int exitcode;
-    char *cmd = NULL;
-    strapp(&cmd, jobcmd(j));              /* jobstate deletes job, so we need to remember it somewhere */
-    int status = jobstate(j, &exitcode);  /* we clean up finished jobs on the fly */
-
-    if ((which == ALL) || (which == status))
-    {
-      if (status == RUNNING)
-        msg("[%d] running '%s'\n", j, cmd);
-      else if (status == STOPPED)
-        msg("[%d] suspended '%s'\n", j, cmd);
-      else
-        if (WIFEXITED(exitcode))
-          msg("[%d] exited '%s', status=%d\n", j, cmd, exitcode);
-        else
-        {
-          if (strcmp(cmd, "false") == 0) /* only for 'false' we want to write something else - otherwise signal 1 is SIHUP */
-            msg("[%d] exited '%s', status=%d\n", j, cmd, WTERMSIG(exitcode));
-          else
-            msg("[%d] killed '%s' by signal %d\n", j, cmd, WTERMSIG(exitcode));
-        }
-    }
-
-    free(cmd);
-
     (void)deljob;
 #endif /* !STUDENT */
   }
 }
 
-/* DESCRIPTION:
- * Monitor job execution. If it gets stopped move it to background.
- * When a job has finished or has been stopped move shell to foreground.
- * INPUT:
- * sigset_t *mask
- * OUTPUT:
- * int - exitcode of job */
-
+/* Monitor job execution. If it gets stopped move it to background.
+ * When a job has finished or has been stopped move shell to foreground. */
 int monitorjob(sigset_t *mask) {
   int exitcode = 0, state;
 
   /* TODO: Following code requires use of Tcsetpgrp of tty_fd. */
 #ifdef STUDENT
-
-  job_t *job = &jobs[0];
-
-  /* foreground job */
-  pid_t shell_pid = getpgrp();
-  setfgpgrp(job->pgid);
-
-  /* wait untill it is finished or stopped - we clean up on the fly if finished */
-  state = jobstate(0, &exitcode);
-  while(state != FINISHED && state != STOPPED)
-  {
-    Sigsuspend(mask);
-    state = jobstate(0, &exitcode);
-  }
-
-  /* move to background */
-  if (state == STOPPED)
-  {
-    int new_j = allocjob();
-    movejob(0, new_j);
-  }
-
-  setfgpgrp(shell_pid);
-
   (void)jobstate;
   (void)exitcode;
   (void)state;
@@ -482,28 +224,6 @@ void shutdownjobs(void) {
 
   /* TODO: Kill remaining jobs and wait for them to finish. */
 #ifdef STUDENT
-
-  for (int j = FG; j < njobmax; j++)
-  {
-    job_t *job = &jobs[j];
-
-    if (job->pgid == 0 || job->state == FINISHED)
-      continue;
-
-    /* we kind of do monitorjob but simplier */
-    if (j > FG)
-      setfgpgrp(job->pgid);
-
-    (void)killjob(j);
-
-    /* we don't need to use jobstate-loop here, 'casuse we do watchjobs later*/
-    while (job->state != FINISHED) 
-      Sigsuspend(&mask);
-
-    if (j > FG)
-      setfgpgrp(getpgrp()); /* give the power to the terminal again */
-  }
-
 #endif /* !STUDENT */
 
   watchjobs(FINISHED);
--- lexer.c
+++ lexer.c
@@ -1,42 +1,21 @@
 #include "shell.h"
 
-/* DESCRIPTION:
- * Easier function to make strings
- * INPUT:
- * char **dstp - destination pointer to string
- * const char *src - source string
- * OUTPUT:
- * none */
-
 void strapp(char **dstp, const char *src) {
   assert(dstp != NULL);
 
-  /* if we have an empty place for string, we just duplicate the source */
-  if (*dstp == NULL)
-  {
+  if (*dstp == NULL) {
     *dstp = strdup(src);
-  }
-  else /* otherwise concatenation */
-  {
+  } else {
     size_t s = strlen(*dstp) + strlen(src) + 1;
     *dstp = realloc(*dstp, s);
     strcat(*dstp, src);
   }
 }
 
-/* DESCRIPTION:
- * Parse command to array of tokens
- * INPUT:
- * char *s - command from shell
- * int *tokc_p - pointer to place for number of tokens
- * OUTPUT:
- * token_t* - array of tokens */
-
 token_t *tokenize(char *s, int *tokc_p) {
   int capacity = 10;
   int ntoks = 0;
 
-  /* allocate space for array */
   token_t *tokvec = malloc(sizeof(token_t) * (capacity + 1));
 
   while (*s != 0) {
@@ -52,7 +31,6 @@ token_t *tokenize(char *s, int *tokc_p) {
       tokvec = realloc(tokvec, sizeof(token_t) * (capacity + 1));
     }
 
-    /* take simple token and put it in array */
     size_t l = strcspn(s, " |&<>;!");
     if (l > 0) {
       tokvec[ntoks++] = s;
@@ -62,7 +40,6 @@ token_t *tokenize(char *s, int *tokc_p) {
 
     token_t tok;
 
-    /* special tokens - replace with pointers to numeric values*/
     if (s[0] == '|') {
       if (s[1] == '|') {
         *s++ = 0;
